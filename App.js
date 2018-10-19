f65e9f6c429648b9f4a0a5225518c5e79f8c5ba3
Wellington Silva da Mota <silvaemota@gmail.com>
2018-10-19 13:04:00 -0300

    Incluído exemplos práticos

--- App.js
+++ App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import CompExample1 from './src/components/example1';
 
-type Props = {};
+class CompExample extends Component {
+  render() {
+    return (
+      <View>
+        <Text>{this.props.propriedade1}</Text>
+        <Text>{this.props.propriedade2}</Text>
+        <Text>{this.props.teste}</Text>
+      </View>
+    );
+  }
+}
 
-export default class App extends Component<Props> {
+export default class App extends Component {
+   constructor(props) {
+    super(props);
+
+    this.state = { texto: 'texto vazio' };
+  }
+
   componentDidMount() {
     SplashScreen.hide();
+  }
+
+  atualizaTexto() {
+    this.setState({ texto: 'Outro texto' });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Bootstrap React Native!</Text>
+        <Text style={styles.example}>Exemplo 1: Props e states</Text>
+        <CompExample propriedade1='ABC' propriedade2='xyz' teste={this.state.texto} />
+        <Button 
+          title='Botão'
+          onPress={() => { this.atualizaTexto(); }}
+        />
+        <CompExample1 />
       </View>
     );
   }
@@ -28,6 +56,10 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
+    margin: 10,
+  },
+  example: {
+    fontSize: 20,
     margin: 10,
   }
 });
---
+++ src/components/example1.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class CompExample1 extends Component {
+  render() {
+    return (
+      <View>
+        <Text>1</Text>
+        <Text>2</Text>
+      </View>
+    );
+  }
+}
+
+export default CompExample1;
